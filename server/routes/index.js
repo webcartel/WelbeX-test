989707e48749e1ac4a698a3854ec91cf2d7e66e6
webcartel <webcartel@yandex.ru>
2022-10-08 21:53:43 +0300

    server

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./db/db.sqlite');
+
 
 router.get('/', function (req, res, next) {
-	res.render('index', { title: 'Express' });
+
+	const result = {}
+
+	db.all('PRAGMA table_info(test_items)', (err, rows) => {
+		if (err) {
+			throw err;
+		}
+
+		result['head'] = rows
+			.filter((row) => {
+				if ( row.name !== 'id' ) {
+					return row.name
+				}
+			})
+			.map((row) => {
+				return row.name
+			})
+
+		db.all("SELECT date, title, quantity, distance FROM test_items WHERE 1", (err, rows) => {
+			if (err) {
+				throw err;
+			}
+	
+			result['body'] = rows
+
+			res.json(result);
+	
+		});
+
+	})
+
+
 });
 
 module.exports = router;
