daa7497aa9a7008129f9bb92991fe31236fc092e
webcartel <webcartel@yandex.ru>
2022-10-09 10:52:49 +0300

    table start

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,24 +1,27 @@
 <template>
 	<div id="app">
-		
+		<Table />
 	</div>
 </template>
 
 
 <script>
+import Table from '@/components/Table';
+
 export default {
+
 	name: 'App',
+
+	components: {
+		Table,
+	},
+
 }
 </script>
 
 
 <style lang="scss">
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
+	
 }
 </style>
